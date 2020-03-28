4dd8db04ec072976ed8c66dc7fdb13b0a73ee327
renggest <ranggadwi.rdw@gmail.com>
2020-03-28 22:35:20 +0700

    upload tugas 1

---
+++ tugas-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream.h>
+using namespace std;
+
+void main(){
+ int luas, panjang, lebar;
+ cout<<"Menghitung luas persegi panjang\n\n";
+ cout<<"Masukkan panjang bangun: "; cin>>panjang; cout<<"\n\n";
+ cout<<"Masukkan lebar bangun: "; cin>>lebar; cout<<"\n\n";
+          luas=panjang*lebar;
+ cout<<"Jadi, Luas persegi panjang yang dicari adalah "<< luas <<" cm";
+ cin>>luas;
+}
